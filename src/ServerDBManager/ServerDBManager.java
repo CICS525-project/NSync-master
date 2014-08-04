6a4b49ca8d69a2f336b9cb0cd13a956575fac542
alikn <alikn@ece.ubc.ca>
2014-08-04 07:55:18 +0000

    adds DBupdate(SendObject) method for updating server DB.

--- src/ServerDBManager/ServerDBManager.java
+++ src/ServerDBManager/ServerDBManager.java
@@ -1,6 +1,8 @@
 package ServerDBManager;
 
 import Controller.SendObject;
+import Controller.SendObject.EventType;
+import java.nio.file.FileSystems;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
@@ -8,6 +10,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -71,11 +74,6 @@ public class ServerDBManager {
         }
     }
 
-    public static boolean updateDB(SendObject sendObject) {
-
-        return false;
-    }
-
     public static boolean isIDInDB(String file_id) {
         boolean result = false;
 
@@ -95,7 +93,15 @@ public class ServerDBManager {
         return result;
     }
 
-    public static int serverInsert(String file_id, String file_path, String file_name, String file_hash, String user_id, java.sql.Timestamp last_local_update) {
+    public static int serverInsert( SendObject obj) {
+        String file_id = obj.getID();
+        String file_path = obj.getFilePath();
+        String file_name = obj.getFileName();
+        String file_hash = obj.getHash();
+        String user_id = obj.getUserID();
+        java.sql.Timestamp last_local_update = getTimeStamp(obj.getTimeStamp());
+        
+        
         int result = -1;
 
         setConnection();
@@ -122,7 +128,12 @@ public class ServerDBManager {
         return result;
     }
 
-    public static int serverModify(String file_id, String file_hash, java.sql.Timestamp last_local_update) {
+    public static int serverModify(SendObject obj) {
+        String file_id = obj.getID();
+        String file_hash = obj.getHash();
+        java.sql.Timestamp last_local_update = getTimeStamp(obj.getTimeStamp());
+                
+                
         int result = -1;
         setConnection();
 
@@ -143,93 +154,66 @@ public class ServerDBManager {
         return result;
     }
 
-    /*
-     public static int serverRename(String file_id, String file_name, String new_file_name, String file_path, java.sql.Timestamp last_local_update, boolean is_folder) 
-     {
-     int result = -1;
-     setConnection();
-     PreparedStatement ps = null;
-     String root_path = "";
+    public static int serverRename(SendObject obj) {
+        String file_id = obj.getID();
+        String file_name = obj.getFileName();
+        String new_file_name = obj.getNewFileName();
+        String file_path = obj.getFilePath();
+        boolean is_folder = obj.isIsAFolder();
+        
+        int result = -1;
+        setConnection();
+        PreparedStatement ps = null;
+        String root_path = "";
 
-     //update the renamed folder/file entry
-     try 
-     {
-     ps = connection.prepareStatement("UPDATE files "
-     + " SET last_local_update = ? "
-     + "    ,file_name = ? "
-     + "WHERE file_path = ? "
-     + "  AND file_name = ?");
-     ps.setTimestamp(1, last_local_update);
-     ps.setString(2, new_file_name);
-     ps.setString(3, file_path);
-     ps.setString(4, file_name);
-     result = ps.executeUpdate();
-     } 
-     catch (Exception e) 
-     {
-     e.printStackTrace();
-     result = -1;
-     }
-     if (is_folder) 
-     {
-     //for folder entry cascade rename to child entries
-     if(file_path.trim().length() == 0)
-     {
-     root_path = "";
-     }
-     else
-     {
-     root_path = file_path + FileSystems.getDefault().getSeparator();
-     }
-     try 
-     {
-     System.out.println("________________________________________________________________________");
-     System.out.println("root_path + file_name + FileSystems.getDefault().getSeparator() ==> "+root_path + file_name + FileSystems.getDefault().getSeparator() );
-     System.out.println("root_path + new_file_name + FileSystems.getDefault().getSeparator() ==> " + root_path + new_file_name + FileSystems.getDefault().getSeparator() );
-     System.out.println("root_path + file_name + FileSystems.getDefault().getSeparator() + % ==> " + root_path + file_name + FileSystems.getDefault().getSeparator() + "%");
-				
-     ps = con.prepareStatement("UPDATE files "
-     + " SET file_path = replace(file_path,  ?, ?) "
-     + "WHERE file_path LIKE ?");
-     ps.setString(1, root_path + file_name + FileSystems.getDefault().getSeparator() );
-     ps.setString(2, root_path + new_file_name + FileSystems.getDefault().getSeparator() );
-     ps.setString(3, root_path + file_name + FileSystems.getDefault().getSeparator() + "%");
-     result = ps.executeUpdate();
-     System.out.println("________________________________________________________________________");
-     } 
-     catch (Exception e) 
-     {
-     e.printStackTrace();
-     result = -1;
-     }
-     try 
-     {
-     System.out.println("________________________________________________________________________");
+        //update the renamed folder/file entry
+        try {
+            ps = connection.prepareStatement("UPDATE files "
+                    + " SET file_name = ? "
+                    + "WHERE file_id = ? ");
 
-     System.out.println("root_path + file_name ==> "+root_path + file_name );
-     System.out.println("root_path + new_file_name ==> " + root_path + new_file_name );
-     System.out.println("root_path + file_name ==> " + root_path + file_name);
-				
-     ps = con.prepareStatement("UPDATE files "
-     + " SET file_path = replace(file_path,  ?, ?) "
-     + "WHERE file_path = ?");
-     ps.setString(1, root_path + file_name);
-     ps.setString(2, root_path + new_file_name);
-     ps.setString(3, root_path + file_name);
-     result = ps.executeUpdate();
-     System.out.println("________________________________________________________________________");
-     } 
-     catch (Exception e) 
-     {
-     e.printStackTrace();
-     result = -1;
-     }
-     }
+            ps.setString(1, new_file_name);
+            ps.setString(2, file_id);
 
-     return result;
-     }
-     */
-    public static int serverDelete(String file_id) {
+            result = ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = -1;
+        }
+        if (is_folder) {
+            //for folder entry cascade rename to child entries
+            if (file_path.trim().length() == 0) {
+                root_path = "";
+            } else {
+                root_path = file_path + "/";
+            }
+            try {
+                System.out.println("________________________________________________________________________");
+                System.out.println("root_path + file_name  ==> " + root_path + file_name);
+                System.out.println("root_path + new_file_name  ==> " + root_path + new_file_name);
+                System.out.println("root_path + file_name + % ==> " + root_path + file_name + "%");
+
+                ps = connection.prepareStatement("UPDATE files "
+                        + " SET file_path = replace(file_path,  ?, ?) "
+                        + "WHERE file_path LIKE ?");
+                ps.setString(1, root_path + file_name);
+                ps.setString(2, root_path + new_file_name);
+                ps.setString(3, root_path + file_name + "%");
+                result = ps.executeUpdate();
+                System.out.println("________________________________________________________________________");
+            } catch (Exception e) {
+                e.printStackTrace();
+                result = -1;
+            }
+
+        }
+
+        return result;
+    }
+
+    public static int serverDelete(SendObject obj) {
+        String file_id = obj.getID();
+        
         int result = -1;
 
         setConnection();
@@ -246,6 +230,39 @@ public class ServerDBManager {
         return result;
     }
 
+    public static java.sql.Timestamp getTimeStamp(Date d) {
+        // java.util.Date today = new java.util.Date();
+        return new java.sql.Timestamp(d.getTime());
+    }
+    
+    /*
+    This is the main method for updating the server DB.
+    It receives a SendObject and updates the DB row associated with the event accordingly.
+    */
+    public static boolean updateDB(SendObject obj) {
+
+        EventType event = obj.getEvent();
+        
+        int result;
+        
+        switch (event){
+            case Create:
+                result = serverInsert(obj);
+                break;
+            case Delete:
+                result = serverDelete(obj);
+                break;
+            case Rename:
+                result = serverRename(obj);
+                break;
+            default:
+                result = serverModify(obj);
+                break;
+        }
+        return result == 1;
+
+    }
+
     private static void setConnection() {
         if (connection == null) {
             startServerDB();
@@ -259,16 +276,24 @@ public class ServerDBManager {
         java.sql.Timestamp TS = new java.sql.Timestamp(new java.util.Date().getTime());
         /*
          System.out.println("testing insert");
-         int result = serverInsert("file_id", "file_path", "file_name", "file_hash", "user_id", TS );
+         int result = serverInsert("file_id3", "file_path/folder1/folder", "file_name", "file_hash", "user_id", TS );
          System.out.println(result);
-         
-        System.out.println("testing modify");
-        int result2 = serverModify("file_id", "file_hash", TS);
+         */
+        /*
+         System.out.println("testing modify");
+         int result2 = serverModify("file_id", "file_hash", TS);
+         System.out.println(result2);
+         */ /*
+         System.out.println("testing delete");
+         int result3 = serverDelete("file_id3");
+         System.out.println(result3);
+         */
+        /*
+        System.out.println("testing rename");
+        int result2 = serverRename("", "folder1", "folder2", "file_path", true);
         System.out.println(result2);
-        */
-        System.out.println("testing delete");
-        int result3 = serverDelete("file_id");
-        System.out.println(result3);
+         */
+
     }
 
 }
